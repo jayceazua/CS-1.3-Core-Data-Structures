3835e80d04d34a35c276aaa50f3b2fc72162d83d
Jayce Azua <jayce.azua@gmail.com>
2019-04-07 21:39:38 -0700

    binary

--- Lessons/source/search.py
+++ Lessons/source/search.py
@@ -4,8 +4,8 @@ def linear_search(array, item):
     """return the first index of item in array or None if item is not found"""
     # implement linear_search_iterative and linear_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
+    # return linear_search_iterative(array, item)
     return linear_search_recursive(array, item)
-    # return linear_search_recursive(array, item)
 
 
 def linear_search_iterative(array, item):
@@ -20,30 +20,49 @@ def linear_search_recursive(array, item, index=0):
     # TODO: implement linear search recursively here
     # once implemented, change linear_search to call linear_search_recursive
     # to verify that your recursive implementation passes all tests
-    if index > (len(array)-1):
+    if index > (len(array) - 1):
         return None
     if array[index] == item:
         return index
     else:
         return linear_search_recursive(array, item, index + 1)
 
+
 def binary_search(array, item):
     """return the index of item in sorted array or None if item is not found"""
     # implement binary_search_iterative and binary_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
+    array = sorted(array)
     return binary_search_iterative(array, item)
     # return binary_search_recursive(array, item)
 
 
 def binary_search_iterative(array, item):
     # TODO: implement binary search iteratively here
-    pass
     # once implemented, change binary_search to call binary_search_iterative
     # to verify that your iterative implementation passes all tests
+    # get the middle index
+    pass
+    
+
+    
 
 
 def binary_search_recursive(array, item, left=None, right=None):
     # TODO: implement binary search recursively here
-    pass
     # once implemented, change binary_search to call binary_search_recursive
     # to verify that your recursive implementation passes all tests
+    pass
+
+
+def main():
+    names = ['Winnie', 'Kojin', 'Brian', 'Nabil', 'Julia', 'Alex', 'Nick']
+    contain = 'Julia'
+    not_contain = 'Jeremy'
+
+    print(binary_search(names, contain))
+    print(binary_search(names, not_contain))
+
+
+if __name__ == '__main__':
+    main()
