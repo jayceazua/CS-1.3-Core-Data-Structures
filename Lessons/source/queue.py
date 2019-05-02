4407351a2b18fd4f7d2b77ace5bc626a63cbdfc8
Jayce Azua <jayce.azua@gmail.com>
2019-05-02 16:27:05 -0400

    array queue done

--- Lessons/source/queue.py
+++ Lessons/source/queue.py
@@ -46,7 +46,7 @@ class LinkedQueue(object): # FILO
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(1) – Why? [TODO] it is just eliminating just the head of the linkedlist"""
         # TODO: Remove and return front item, if any
         if self.is_empty():
             raise ValueError('No one on queue')
@@ -75,29 +75,41 @@ class ArrayQueue(object):
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
         # TODO: Check if empty
+        return len(self.list) == 0
 
     def length(self):
         """Return the number of items in this queue."""
         # TODO: Count number of items
+        return len(self.list)
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(1) – Why? [TODO] it just adds to the end"""
         # TODO: Insert given item
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
         # TODO: Return front item, if any
+        if self.is_empty():
+            return None
+        return self.list[0]
+
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(n) – Why? [TODO] because once the item is removed from the front it needs to shift all elements over one by one."""
         # TODO: Remove and return front item, if any
+        if self.is_empty():
+            raise ValueError("Nothing to queue")
+        original_front = self.front()
+        self.list.pop(0)
+        return original_front
 
 
 # Implement LinkedQueue and ArrayQueue above, then change the assignment below
 # to use each of your Queue implementations to verify they each pass all tests
-Queue = LinkedQueue
-# Queue = ArrayQueue
+# Queue = LinkedQueue
+Queue = ArrayQueue
