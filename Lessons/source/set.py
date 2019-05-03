eea8a4fe657ec42f33ecf44a7a1867c260d4a268
Jayce Azua <jayce.azua@gmail.com>
2019-05-03 15:38:15 -0400

    [add] doing sets with hashtables

--- Lessons/source/set.py
+++ Lessons/source/set.py
@@ -1,8 +1,41 @@
 from hashtable import HashTable
 
-
 class HashSet (object):
-  pass
+  # initializer 
+  def __init__(self, elements=None):
+    self.elements = HashTable()
+    self.size = 0
+    if elements == None:
+      for key, value in elements:
+        self.size += self.elements.add(key, value)
+
+  def __iter__(self):
+        for element in self.elements:
+            yield element
+
+  def get_elements(self):
+    pass
+  def length(self):
+    pass
+  def contains(self, element):
+    pass
+  def add(self, element):
+    pass
+  def remove(self, element):
+    pass
+  def union(self, other_set):
+    pass
+  def intersection(self, other_set):
+    pass
+  def difference(self, other_set):
+    pass
+  def is_subset(self, other_set):
+    pass
+# class Set(HashTable):
+#     def __init__(self, elements=None):
+#         pass
+
+
 
 
 if __name__ == "__main__":
