3eef27675c3c8a9003b693b31ab3c7a58b5e6aea
Jayce Azua <jayce.azua@gmail.com>
2019-05-04 21:44:37 -0400

    [part 1] done need to write test

--- Lessons/source/set.py
+++ Lessons/source/set.py
@@ -3,15 +3,36 @@ from hashtable import HashTable
 class HashSet (object):
   # initializer 
   def __init__(self, elements=None):
-    self.elements = HashTable()
-    self.size = 0
+    self.container = HashTable()
+    self.size = self.container.size
+    if elements is not None:
+      print(elements)
+      for item in elements:
+        self.size += 1
+        self.container.set(item, None)
+
+  def __str__(self):
+    """Return a formatted string representation of this hash table."""
+    items = ["{!r}".format(key) for key, _ in self.container.items()]
+    return "{" + ", ".join(items) + "}
+
+  def __iter__(self):
+    return self.container.__iter__()
 # Part 1
   def add(self, element):
-    pass
+    if self.contains(element):
+      raise ValueError("Element already exist")
+    self.size += 1
+    self.container.set(element, None)
+
   def remove(self, element):
-    pass
+    if self.contains(element):
+      self.size -= 1
+      self.container.delete(element)
+    raise ValueError("Element does not exist")
+
   def contains(self, element):
-    pass
+    return self.container.contains(element)
 # Part 2
   def union(self, other_set):
     pass
