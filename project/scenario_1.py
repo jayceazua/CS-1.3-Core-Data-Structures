b93f8e641aa0f3265819b88fbdc5f3e9aa2c83da
Jayce Azua <jayce.azua@gmail.com>
2019-05-11 17:40:30 -0700

    [almost done]

--- project/scenario_1.py
+++
@@ -1,4 +0,0 @@
-#  SCENARIO 1
-# One-time route cost check:
-# You have a carrier route list with 100,000 (100k) entries (in arbitrary order) and a single phone number.
-# How quickly can you find the cost of calling this number?
